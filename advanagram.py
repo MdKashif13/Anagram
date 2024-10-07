f0bfdca6184d1570ade581342f0ad4088000b26d
MdKashif13 <136237666+MdKashif13@users.noreply.github.com>
2024-10-07 12:14:53 +0530

    Add files via upload

---
+++ advanagram.py
@@ -0,0 +1,17 @@
+lstfile=open("wlist","r",encoding="utf-8")
+lst=[]
+for i in lstfile:
+	lst.append(i)
+nwlst=[]
+for x in lst:
+	nwlst.append(x.replace("\n",""))
+while True:
+	word=(input(">>"))
+	word=word.lower()
+	for a in range(len(nwlst)):
+		if len(word)==len(nwlst[a]):
+			if sorted(word)==sorted(nwlst[a]):
+				print("Match Found ->",nwlst[a])
+
+
+
